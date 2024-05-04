dec074fb1d5a5834fbcddc7932c5d83e4ff911e4
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2024-05-04 15:51:26 +0100

    update

--- Components/header.php
+++ Components/header.php
@@ -11,13 +11,10 @@ namespace SkeletonPHP\Components\Header;
 	<meta name="keywords" content="put, keywords, here" />
 	<meta name="robots" content="noindex" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<title>PHP-MySQL Website from Scratch</title>
-
+	<title>Skeleton-PHP MySQL Website from Scratch</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-
 	<link rel="stylesheet" href="style.css">
 </head>
