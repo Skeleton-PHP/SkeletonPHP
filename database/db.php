ba64107f695fea55c5714ba5ee07bbf790fedff3
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2024-05-07 20:51:37 +0100

    Whitespacing fix

--- database/db.php
+++ database/db.php
@@ -12,7 +12,7 @@ $password = $conf->getPassword();
 $dbname = $conf->getDatabase();
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
