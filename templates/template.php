ec8798e056dd19174d44d99b390d8e00650dbc82
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2024-05-08 19:57:33 +0100

    Trying to get the template system to work.

--- templates/template.php
+++ templates/template.php
@@ -1,94 +1,110 @@
 <?php
 namespace SkeletonPHP\Templates;
-/**
- * @property string cachedPath
- * @property bool cacheEnabled
- * @property array blocks
- */
-class Template {
 
-    private $blocks = array();
-    private $cachedPath = 'cache/';
+class Stencil
+{
+    private $blocks = [];
+    private $cachePath = 'cache/';
     private $cacheEnabled = false;
 
-    public function view($file, $data = array()) {
-		$cached_file = self::cache($file);
-	    extract($data, EXTR_SKIP);
-	   	require $cached_file;
-	}
+    public function view($file, $data = [])
+    {
+        $cachedFile = $this->cache($file);
+        extract($data, EXTR_SKIP);
+        require $cachedFile;
+    }
 
-    public function cache($file) {
-		if (!file_exists(self::$cachedPath)) {
-		  	mkdir(self::$cachedPath, 0744);
-		}
-	    $cached_file = self::$cachedPath . str_replace(array('/', '.html'), array('_', ''), $file . '.php');
-	    if (!self::$cacheEnabled || !file_exists($cached_file) || filemtime($cached_file) < filemtime($file)) {
-			$code = self::includeFiles($file);
-			$code = self::compileCode($code);
-	        file_put_contents($cached_file, '<?php class_exists(\'' . __CLASS__ . '\') or exit; ?>' . PHP_EOL . $code);
-	    }
-		return $cached_file;
-	}
+    private function cache($file)
+    {
+        if (!file_exists($this->cachePath)) {
+            mkdir($this->cachePath, 0744, true);
+        }
 
-	public function clearCache() {
-		foreach(glob(self::$cachedPath . '*') as $file) {
-			unlink($file);
-		}
-	}
+        $cachedFile = $this->cachePath . str_replace(['/', '.html'], ['_', ''], $file . '.php');
 
-	public function compileCode($code) {
-		$code = self::compileBlock($code);
-		$code = self::compileYield($code);
-		$code = self::compileEscapedEchos($code);
-		$code = self::compileEchos($code);
-		$code = self::compilePHP($code);
-		return $code;
-	}
+        if (!$this->cacheEnabled || !file_exists($cachedFile) || filemtime($cachedFile) < filemtime($file)) {
+            $code = $this->includeFiles($file);
+            $code = $this->compileCode($code);
+            file_put_contents($cachedFile, '<?php class_exists(\'' . __CLASS__ . '\') or exit; ?>' . PHP_EOL . $code);
+        }
 
-	public function includeFiles($file) {
-		$code = file_get_contents($file);
-		preg_match_all('/{% ?(extends|include) ?\'?(.*?)\'? ?%}/i', $code, $matches, PREG_SET_ORDER);
-		foreach ($matches as $value) {
-			$code = str_replace($value[0], self::includeFiles($value[2]), $code);
-		}
-		$code = preg_replace('/{% ?(extends|include) ?\'?(.*?)\'? ?%}/i', '', $code);
-		return $code;
-	}
+        return $cachedFile;
+    }
 
-	public function compilePHP($code) {
-		return preg_replace('~\{%\s*(.+?)\s*\%}~is', '<?php $1 ?>', $code);
-	}
+    public function clearCache()
+    {
+        foreach (glob($this->cachePath . '*') as $file) {
+            unlink($file);
+        }
+    }
 
-    public function compileEchos($code) {
-		return preg_replace('~\{{\s*(.+?)\s*\}}~is', '<?php echo $1 ?>', $code);
-	}
+    private function compileCode($code)
+    {
+        $code = $this->compileBlock($code);
+        $code = $this->compileYield($code);
+        $code = $this->compileEscapedEchos($code);
+        $code = $this->compileEchos($code);
+        $code = $this->compilePHP($code);
+        return $code;
+    }
 
-    public function compileEscapedEchos($code) {
-		return preg_replace('~\{{{\s*(.+?)\s*\}}}~is', '<?php echo htmlentities($1, ENT_QUOTES, \'UTF-8\') ?>', $code);
-	}
+    private function includeFiles($file)
+    {
+        $code = file_get_contents($file);
+        preg_match_all('/{% ?(extends|include) ?\'?(.*?)\'? ?%}/i', $code, $matches, PREG_SET_ORDER);
 
-    public function compileBlock($code) {
-		preg_match_all('/{% ?block ?(.*?) ?%}(.*?){% ?endblock ?%}/is', $code, $matches, PREG_SET_ORDER);
-		foreach ($matches as $value) {
+        foreach ($matches as $value) {
+            $code = str_replace($value[0], $this->includeFiles($value[2]), $code);
+        }
 
-		if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
-			if (strpos($value[2], '@parent') === false) {
-				self::$blocks[$value[1]] = $value[2];
-			} else {
-				self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
-			}
-			$code = str_replace($value[0], '', $code);
-		}
+        $code = preg_replace('/{% ?(extends|include) ?\'?(.*?)\'? ?%}/i', '', $code);
+        return $code;
+    }
 
-		return $code;
-	}
+    private function compilePHP($code)
+    {
+        return preg_replace('~\{%\s*(.+?)\s*\%}~is', '<?php $1 ?>', $code);
+    }
 
-    public function compileYield($code) {
-		foreach(self::$blocks as $block => $value) {
-			$code = preg_replace('/{% ?yield ?' . $block . ' ?%}/', $value, $code);
-		}
-		$code = preg_replace('/{% ?yield ?(.*?) ?%}/i', '', $code);
-		return $code;
-	}
+    private function compileEchos($code)
+    {
+        return preg_replace('~\{{\s*(.+?)\s*\}}~is', '<?php echo $1 ?>', $code);
+    }
 
+    private function compileEscapedEchos($code)
+    {
+        return preg_replace('~\{{{\s*(.+?)\s*\}}}~is', '<?php echo htmlentities($1, ENT_QUOTES, \'UTF-8\') ?>', $code);
+    }
+
+    private function compileBlock($code)
+    {
+        preg_match_all('/{% ?block ?(.*?) ?%}(.*?){% ?endblock ?%}/is', $code, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $value) {
+            if (!array_key_exists($value[1], $this->blocks)) {
+                $this->blocks[$value[1]] = '';
+            }
+
+            if (strpos($value[2], '@parent') === false) {
+                $this->blocks[$value[1]] = $value[2];
+            } else {
+                $this->blocks[$value[1]] = str_replace('@parent', $this->blocks[$value[1]], $value[2]);
+            }
+
+            $code = str_replace($value[0], '', $code);
+        }
+
+        return $code;
+    }
+
+    private function compileYield($code)
+    {
+        foreach ($this->blocks as $block => $value) {
+            $code = preg_replace('/{% ?yield ?' . $block . ' ?%}/', $value, $code);
+        }
+
+        $code = preg_replace('/{% ?yield ?(.*?) ?%}/i', '', $code);
+        return $code;
+    }
 }
+?>
