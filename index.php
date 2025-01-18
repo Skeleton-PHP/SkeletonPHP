35be62c317c7966269694a4d6a7ac441c13dcef3
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2025-01-18 17:51:56 +0000

    update

--- index.php
+++ index.php
@@ -9,12 +9,7 @@ $router = new \Bramus\Router\Router();
 
 
 $router->get('/', function() {
-<<<<<<< HEAD
-
     echo 'Home Page Contents';
-=======
-    include_once ('');
->>>>>>> 96edd88b223cdaf12413a02ce4f94b6796593ce8
 });
 $router->get('/about', function() {
     echo 'About Page Contents';
