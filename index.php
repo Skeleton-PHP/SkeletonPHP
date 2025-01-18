5e5845fe0864498a54100b0c3062d060965d9666
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2025-01-18 17:14:12 +0000

    Commit

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 namespace SkeletonPHP;
 
 require_once __DIR__ . '/vendor/autoload.php';
-
+require_once __DIR__ . '/controller.php';
+require_once __DIR__ . '/';
 $router = new \Bramus\Router\Router();
 
 
 $router->get('/', function() {
+
     echo 'Home Page Contents';
 });
 $router->get('/about', function() {
---
+++ views/Home/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.png">
+    <title>Skeleton PHP</title>
+</head>
+
+<body>
+
+    <section>
+        <h1>Homepage</h1>
+        <p> This is a test page.</p>
+    <section>
+</body>
+
+</html>
