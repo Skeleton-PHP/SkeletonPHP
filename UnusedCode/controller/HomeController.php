b7846e768a52bf3b363dfb8bf5afb675de1486bc
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2025-01-18 17:55:27 +0000

    Update

---
+++ UnusedCode/controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+namespace SkeletonPHP\Controllers;
+
+use Cv\Models\Resume;
+
+class ResumeController extends Controller{
+
+//Logic for parsing information once its collected from the db
+    public static function index()
+    {
+
+    }
+}
