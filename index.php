00d68d67b6a4971ffa342140aec97a55ae33a4f2
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2025-04-10 20:04:58 +0100

    Added model & controller

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
-
 namespace SkeletonPHP;
+
+use SkeletonPHP\view\Home;
+use SkeletonPHP\view\About;
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/controller.php';
+
 $router = new \Bramus\Router\Router();
 
+// Instantiate the Home view and define its route
+$homeView = new Home();
+$router->get('/', function() use ($homeView) {
+    echo $homeView->render();
+});
 
-$router->get('/', function() {
-    echo 'Home Page Contents';
+// Optionally, instantiate the About view and define its route
+$aboutView = new About();
+$router->get('/about', function() use ($aboutView) {
+    echo $aboutView->render();
 });
-$router->get('/about', function() {
-    echo 'About Page Contents';
-});
+
 $router->run();
 ?>
